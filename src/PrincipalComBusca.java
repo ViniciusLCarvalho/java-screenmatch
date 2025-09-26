03b07f33e0c5500a59855e5e777bb85d52193bd6
Vinicius Carvalho <vinicius.carvalho1@outlook.com.br>
2025-09-26 17:30:37 -0300

    Correcting folders and files

--- src/PrincipalComBusca.java
+++ src/PrincipalComBusca.java
@@ -1,4 +1,9 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.URI;
 import java.net.URLEncoder;
 import java.net.http.HttpClient;
@@ -9,10 +14,26 @@ import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
+        String chave = null;
         Scanner input = new Scanner(System.in);
+
+        InputStream fis = new FileInputStream("apiKey.txt");
+        Reader isr = new InputStreamReader(fis, "UTF-8");
+        BufferedReader br = new BufferedReader(isr);
+
+        String linha = br.readLine();
+
+        while (linha != null) {
+            if (linha.startsWith("APIKEYOMDB")) {
+                chave = linha.split("=")[1];
+                break;
+            }
+            linha = br.readLine();
+        }
+        br.close();
+
         System.out.println("Digite um filme para buscar: ");
         var busca = URLEncoder.encode(input.nextLine(), StandardCharsets.UTF_8);
-        String chave = "f0d96f05";
         String endereco = "https://www.omdbapi.com/?t="+ busca + "&apikey=" + chave;
 
         HttpClient client = HttpClient.newHttpClient();
